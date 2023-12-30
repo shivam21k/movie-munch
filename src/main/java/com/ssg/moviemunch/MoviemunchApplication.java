203d0545f8e91847ec01dee849ceb45189b8fdd4
shivam21k <satyamshivam.sp@gmail.com>
2023-12-30 20:28:18 +0530

    initial commit

---
+++ src/main/java/com/ssg/moviemunch/MoviemunchApplication.java
@@ -0,0 +1,13 @@
+package com.ssg.moviemunch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoviemunchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MoviemunchApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ssg/moviemunch/MoviemunchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ssg.moviemunch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoviemunchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
